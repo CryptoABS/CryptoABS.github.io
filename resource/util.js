b73210997873465e4adad2bdbcca03b7a9700f78
Gen Black <gen.black@gmail.com>
2017-07-12 16:20:55 +0800

    Factorize all JS code from html

--- resource/util.js
+++
@@ -1,4 +0,0 @@
-$("select.language-switcher").change(() => {
-  let locale = $("select.language-switcher option:selected").val();
-  window.location.assign(locale == "en-US" ? `./` : `./index_${locale}.html`);
-});
