4edbfe829951784324046ae2989ffb1d326cb13c
Yan <laiyanlong@gmail.com>
2017-07-14 10:46:08 +0800

    fix copyboard

--- resource/finalize.js
+++ resource/finalize.js
@@ -43,57 +43,6 @@ function subscribeEmail() {
   }
 }
 
-function copyToClipboard(elem) {
-	  // create hidden text element, if it doesn't already exist
-    var targetId = "_hiddenCopyText_";
-    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-    var origSelectionStart, origSelectionEnd;
-    if (isInput) {
-        // can just use the original source element for the selection and copy
-        target = elem;
-        origSelectionStart = elem.selectionStart;
-        origSelectionEnd = elem.selectionEnd;
-    } else {
-        // must use a temporary form element for the selection and copy
-        target = document.getElementById(targetId);
-        if (!target) {
-            var target = document.createElement("textarea");
-            target.style.display = "none";
-            target.style.position = "absolute";
-            target.style.left = "-9999px";
-            target.style.top = "0";
-            target.id = targetId;
-            document.body.appendChild(target);
-        }
-        target.textContent = elem.textContent;
-    }
-    // select the content
-    var currentFocus = document.activeElement;
-    target.focus();
-    target.setSelectionRange(0, target.value.length);
-    
-    // copy the selection
-    var succeed;
-    try {
-    	  succeed = document.execCommand("copy");
-    } catch(e) {
-        succeed = false;
-    }
-    // restore original focus
-    if (currentFocus && typeof currentFocus.focus === "function") {
-        currentFocus.focus();
-    }
-    
-    if (isInput) {
-        // restore prior selection
-        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
-    } else {
-        // clear temporary content
-        target.textContent = "";
-    }
-    return succeed;
-}
-
 /**
  * Various variables
  */
@@ -150,6 +99,13 @@ $('a[href="#investModal"]').click(function(event) {
   });
 });
 
-$('#contractAddressCopy').click(function() {
-  copyToClipboard(document.getElementById("contractAddress"));
+/**
+ * use Clipboard of contractAddress
+ */
+
+const contractAddress = '0xfd95392e1ce28a6debff90feb0a28a1392df738b';
+var clipboard = new Clipboard('#contractAddressCopy', {
+    text: function() {
+        return contractAddress;
+    }
 });
